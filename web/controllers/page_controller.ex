1fb6559b3d695ea10d6bdc72a24452912b37ef57
Andreas Sliwka <andreas.sliwka@evopark.de>
2016-09-02 18:42:12 +0200

    Execute mix phoenix.new emporium --no-brunch

---
+++ web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule Emporium.PageController do
+  use Emporium.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
---
+++ web/router.ex
@@ -0,0 +1,26 @@
+defmodule Emporium.Router do
+  use Emporium.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Emporium do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Emporium do
+  #   pipe_through :api
+  # end
+end
